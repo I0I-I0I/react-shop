9d27b92cf454217d856d204efe17e5be600a98e3
I0I-I0I <azudjndj@gmail.com>
2024-08-26 14:17:10 +0300

    Update: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-function App() {
-  return (
-	<h1>I work</h1>
-  )
-}
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Link from "./components/UI/Link/Link";
+import styles from "./App.module.css";
 
-export default App
+const App = () => {
+	return (
+		<div className={styles.root}>
+			<Header />
+			<main>
+				<Link href="#" variant="default">Hello</Link>
+			</main>
+			<Footer />
+		</div>
+	);
+};
+
+export default App;
