abdf1ccc0b9f58dca0d94d15ad48f0523f0e11a1
I0I-I0I <azudjndj@gmail.com>
2024-08-27 22:43:56 +0300

    Change: Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,93 @@
-import { Link, Typography } from "@/components/UI";
+import { Link, Dropdown, Logo, TextWithIcon, Search } from "@/components/UI";
+import { LinkDropdown } from "./LinkDropdown/LinkDropdown";
 import styles from "./Header.module.css";
+import cls from "@/utils/cls";
 
-export const Header = () => {
-	return (
-		<header className={styles.header}>
-			<div className="logo">
-				<img src="" alt="Logo" />
-			</div>
-			<Typography variant="title" tag="h1">Hello</Typography>
-			<nav className="nav header__nav">
-				<ul className="nav__list">
-					<li className="nav__item">
-						<Link variant="small" className="nav__link" href="#">
-							Home
-						</Link>
+const data = {
+	languages: [
+		"English",
+		"Russian",
+		"Bulgar",
+		"Ukrain"
+	],
+	currency: [
+		"USD",
+		"RUB"
+	]
+}
+
+export const Header = () => (
+	<header className={styles.header}>
+		<div className={styles.header__top}>
+			<ul className={cls(styles.contacts__list, styles.header__list)}>
+				<li className={styles.contacts__item}>
+					<Link href="mailto:mhhasanul@gmail.com" className={styles.contacts__iconWrapper}>
+						<TextWithIcon src="/icons/mail.svg" direction="left">mhhasanul@gmail.com</TextWithIcon>
+					</Link>
+				</li>
+				<li className={styles.contacts__itemWrapper}>
+					<Link href="tel:(12345)67890" className={styles.contacts__iconWrapper}>
+						<TextWithIcon src="/icons/phone.svg" direction="left">(12345)67890</TextWithIcon>
+					</Link>
+				</li>
+			</ul>
+			<ul className={cls(styles.dropdown__list, styles.header__list)}>
+				<li className={styles.dropdown__item}>
+					<Dropdown options={data.languages}></Dropdown>
+				</li>
+				<li className={styles.dropdown__item}>
+					<Dropdown options={data.currency}></Dropdown>
+				</li>
+			</ul>
+			<ul className={styles.header__list}>
+				<li className={styles.header__item}>
+					<Link href="#">
+						<TextWithIcon src="/icons/user.svg" gap="2px">Login</TextWithIcon>
+					</Link>
+				</li>
+				<li className={styles.header__item}>
+					<Link href="#">
+						<TextWithIcon src="/icons/heart-alt.svg" gap="2px">Wishlist</TextWithIcon>
+					</Link>
+				</li>
+				<li className={styles.header__item}>
+					<Link className="" href="#" aria-label="Card">
+						<TextWithIcon src="/icons/card.svg" gap="0px" size="medium"><span className="visually-hidden">Card</span></TextWithIcon>
+					</Link>
+				</li>
+			</ul>
+		</div>
+		<div className={styles.header__bottom}>
+			<Logo />
+			<nav className={styles.nav}>
+				<ul className={cls(styles.nav__list)}>
+					<li>
+						<LinkDropdown data={[
+								{ value: "Hero", link: "#" },
+								{ value: "About", link: "#" },
+								{ value: "What", link: "#" },
+								{ value: "Idk", link: "#" },
+							]}
+						/>
 					</li>
-					<li className="nav__item">
-						<Link className="nav__link" href="#">
-							Products
-						</Link>
+					<li>
+						<Link href="#">Pages</Link>
 					</li>
-					<li className="nav__item">
-						<Link className="nav__link" href="#">
-							Account
-						</Link>
+					<li>
+						<Link href="#">Products</Link>
 					</li>
-					<li className="nav__item">
-						<Link className="nav__link" href="#">
-							Card
-						</Link>
+					<li>
+						<Link href="#">Blog</Link>
+					</li>
+					<li>
+						<Link href="#">Shop</Link>
+					</li>
+					<li>
+						<Link href="#">Contacts</Link>
 					</li>
 				</ul>
 			</nav>
-		</header>
-	);
-};
+			<Search />
+		</div>
+	</header>
+);
