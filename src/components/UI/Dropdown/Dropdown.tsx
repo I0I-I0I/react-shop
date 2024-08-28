34220e955c824fd1265d3d2931d716aa11ddd764
I0I-I0I <azudjndj@gmail.com>
2024-08-28 21:06:18 +0300

    Change: rewrite <Dropdown /> component

--- src/components/UI/Dropdown/Dropdown.tsx
+++ src/components/UI/Dropdown/Dropdown.tsx
@@ -1,19 +1,77 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./Dropdown.module.css";
+import cls from "@/utils/cls";
+
+type OpenOnType =
+	| "hover"
+	| "click";
 
 interface DropdownProps {
-	label?: string;
 	options: string[];
+	openOn?: OpenOnType;
 }
 
-export const Dropdown: React.FC<DropdownProps> = ({ label, options }) => (
-	<div className={styles.dropdown__block}>
-		{ label && <label htmlFor="cars">{label}</label> }
-		<select className={styles.dropdown} name="cars" id="cars">
-			{
-				options.map((option) =>
-				<option value={option}>{option}</option>
-			)}
-		</select>
-	</div>
-)
+interface IMode {
+	dropdown?: {
+		onMouseOver: () => void;
+		onMouseLeave: () => void;
+	};
+	currentItem?: {
+		onClick: () => void;
+	};
+}
+
+export const Dropdown = ({ options, openOn = "click" }: DropdownProps) => {
+	const [currentItem, setCurrentItem] = useState(options[0]);
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleOnClick = (item: string) => {
+		setIsOpen(false);
+		setCurrentItem(item);
+	};
+
+	const modeList = {
+		"hover":{
+			dropdown: {
+				onMouseOver: () => setIsOpen(true),
+				onMouseLeave: () => setIsOpen(false),
+			}
+		},
+		"click":{
+			currentItem: {
+				onClick: () => setIsOpen((state) => !state),
+			}
+		}
+	};
+
+	const mode: IMode = modeList[openOn];
+
+	return (
+		<div
+			className={styles.dropdown}
+			{...mode.dropdown}
+		>
+			<button
+				className={cls(styles.item, styles.currentItem)}
+				{...mode.currentItem}
+			>
+				{currentItem}
+			</button>
+			{ isOpen &&
+				<ul className={cls(styles.dropdownContent)}>
+					{
+						options.map((option) => (
+							<li className={styles.dropdownItem}>
+								<button
+									onClick={() => handleOnClick(option)}
+									className={styles.item}
+								>{option}
+								</button>
+							</li>
+						)
+					)}
+				</ul>
+			}
+		</div>
+	)
+}
